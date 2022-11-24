f8cd74ec55b1d6814a673f58825c4974f69fee72
Pedro Paulo Boaventura Grein <pedro.grein@gmail.com>
2022-11-24 13:42:53 -0300

    header

--- header.php
+++ header.php
@@ -1,17 +1,16 @@
 <!-- Header -->
 <header class="container">
-	<div class="row justify-content-between menu-row">
 		
 		<!-- Sidebar Esquerdo Mobile -->
-		<button class="menu-esquerdo__toggle col-3 d-sm-block d-md-none" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="sidebar__esquerda" aria-expanded="false" aria-label="Menu esquerdo toggle">
+		<button class="menu-esquerdo__toggle" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="sidebar__esquerda" aria-expanded="false" aria-label="Menu esquerdo toggle">
 			<i class="menu-btn-toggle fas fa-bars"></i>
 		</button>
 		<!-- Fim Sidebar Esquerdo Mobile -->
 
 		<!-- Marca IFPR -->
-		<a href="<?php echo $home_url; ?>" class="col-6 col-md-4 logo__link">
+		<a href="<?php echo $home_url; ?>" class="marca-ifpr">
 			<?php $campus = get_bloginfo(); ?>
-			<svg id="logo_ifpr" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 230 54" role="img" aria-label="[title + description]">
+			<svg id="marca-ifpr" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 230 54" role="img" aria-label="[title + description]">
 				<title>Instituto Federal do Paran&aacute;</title>
 				<desc>Marca do IFPR</desc>
 				<path class="st5" d="M37.4 36.4c0 0.6-0.5 1.1-1 1.1h-8.7c-0.6 0-1.1-0.5-1.1-1.1v-8.7c0-0.6 0.5-1.1 1.1-1.1h8.7c0.6 0 1.1 0.5 1.1 1.1V36.4z"/>
@@ -34,32 +33,24 @@
 				}
 				?>
 				<text transform="matrix(1.0017 0 0 1 <?php echo $transformLine1; ?>)">
-					<tspan class="st0 st1 st2">  I</tspan>
-					<tspan x="5" class="st0 st1 st2">  NS</tspan>
-					<tspan x="25.7" class="st0 st1 st2">  TIT</tspan>
-					<tspan x="48.3" class="st0 st1 st2">  U</tspan>
-					<tspan x="59.7" class="st0 st1 st2">  T</tspan>
-					<tspan x="68.5" class="st0 st1 st2">  O F</tspan>
-					<tspan x="92.8" class="st0 st1 st2">  EDER</tspan>
-					<tspan x="131" class="st0 st1 st2">  A</tspan>
-					<tspan x="141.4" class="st0 st1 st2">  L</tspan>
+					<tspan class="st0">INSTITUTO FEDERAL</tspan>
 				</text>
-				<text transform="matrix(1.0017 0 0 1 <?php echo $transformLine2; ?>)" class="st0 st3 st4">  Paraná</text>
+				<text transform="matrix(1.0017 0 0 1 <?php echo $transformLine2; ?>)" class="st1">Paraná</text>
 				<?php if ($campus == "Instituto Federal do Paraná"){
 					}else{?>
-				<text transform="matrix(1.0017 0 0 1 43.9326 50.5059)" class="st0 st3 st4">  <?php echo $campus; ?></text>
+				<text transform="matrix(1.0017 0 0 1 43.9326 50.5059)" class="st2"><?php echo $campus; ?></text>
 				<?php }; ?>
 			</svg>
 		</a>
 		<!-- Fim Marca IFPR -->
 		
 		<!-- Busca -->
-		<button class="col-3 d-sm-block d-md-none busca-btn" type="button" data-toggle="collapse" data-target="#busca" aria-controls="busca" aria-expanded="false" aria-label="Busca toggle">
+		<button class="busca-btn" type="button" data-toggle="collapse" data-target="#busca" aria-controls="busca" aria-expanded="false" aria-label="Busca toggle">
 		<i class="fas fa-search text-center busca__icone_mobile align-self-center"></i>
 		</button>
 		
-		<div id="busca" class="col-8 d-none d-md-block">
-			<form role="search" method="get" class="search-form text-right" action="<?php echo home_url( '/' ); ?>">
+		<div id="busca" class="header-busca">
+			<form role="search" method="get" class="search-form" action="<?php echo home_url( '/' ); ?>">
 				<label class="mb-0">
         			<span class="screen-reader-text"><?php echo _x( 'Pesquisar por:', 'label', 'ifpr_theme' ); ?></span>
        				 <input type="search" class="busca__texto_input" 
@@ -69,12 +60,11 @@
     			</label>
     			<button type="submit" class="btn busca__botao" value="Submit"><i class="fas fa-search"></i></button>
 			</form>
-			<p class="mb-0 text-right busca__link_sei"><?php _e( 'Procure', 'ifpr_theme' ); ?> <span class="font-weight-bold"><?php _e( 'documentos institucionais', 'ifpr_theme' ); ?></span> <?php _e('no', 'ifpr_theme'); ?> <a href="https://sei.ifpr.edu.br/sei/modulos/pesquisa/md_pesq_processo_pesquisar.php?acao_externa=protocolo_pesquisar&acao_origem_externa=protocolo_pesquisar&id_orgao_acesso_externo=0"><span class="badge badge-ifpr"><?php _e( 'SEI!', 'ifpr_theme' ); ?><i class="fas fa-external-link-alt"></i></span></a></p>
+			<p class="busca__link_sei">Documentos institucionais estão no <a href="https://sei.ifpr.edu.br/sei/modulos/pesquisa/md_pesq_processo_pesquisar.php?acao_externa=protocolo_pesquisar&acao_origem_externa=protocolo_pesquisar&id_orgao_acesso_externo=0"> SEI!</a></p>
 		</div>
 		<!-- Fim Busca -->
 
 		<?php
-			//Nav dinâmico para /en-US /es-ES e /pt-BR
 			$home_url = null;
 				wp_nav_menu( 
 					array(
@@ -89,6 +79,5 @@
 					'theme_location' => 'nav-superior' 
 				));
 		?>
-	</div>
 </header>
 <!-- Fim Header -->
